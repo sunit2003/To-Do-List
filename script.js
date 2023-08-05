fbd3ec2dc51af5702c8dbf166d79cf4eb8a8a17a
Sunit Gupta <sunit2003@gmail.com>
2023-08-05 17:57:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const inputBox=document.getElementById("inputbox");
+const listContainer=document.getElementById("listcontainer");
+function addtask(){
+    if(inputBox.value === ''){
+        alert("Please Add Some Task");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\uD83D\uDFA9";
+        li.appendChild(span);
+    }
+    inputBox.value="";
+    savedata();
+}
+listContainer.addEventListener('click',function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        savedata();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        savedata();
+    }
+
+}, false)
+
+function savedata(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showdata(){
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+showdata();
